423fad4841193c4894844e56ceeae7075334785d
VictorNeb <victornebun05@gmail.com>
2025-03-28 00:19:58 +0200

    Update BlogCard.tsx

--- src/components/blog/BlogCard.tsx
+++ src/components/blog/BlogCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { formatDate } from 'date-fns';
 import type { NotionPost } from '@/lib/notion';
